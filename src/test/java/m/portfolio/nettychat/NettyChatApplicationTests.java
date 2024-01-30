b78f0c440f99577231516f6813f49136d222f7d7
ghghwldk <ghghwldk@gmail.com>
2024-01-30 22:45:58 +0900

    after initialization of the project

---
+++ src/test/java/m/portfolio/nettychat/NettyChatApplicationTests.java
@@ -0,0 +1,13 @@
+package m.portfolio.nettychat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
